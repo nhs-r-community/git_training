c7bf1d93fd7bea29a2d705eed2a9e17c047b0c60
Helen R <58853548+helrich@users.noreply.github.com>
2022-07-28 16:55:21 +0100

    Update example_script.r

--- exercises/example_script.r
+++ exercises/example_script.r
@@ -1,1 +1,30 @@
 # contains example r script
+
+# install tidyverse and run
+install.packages(c("tidyverse", "NHSRdatasets")
+library(tidyverse)
+library(NHSRdatasets)
+
+# assing a string to a variable
+myString <- "Hello, World!"
+
+# print the variable
+print(myString)
+
+# bar chart for different types of diamonds and levels of clarity using the diamonds dataset
+ggplot(data = diamonds) +
+  geom_bar(
+    mapping = aes(x = cut, fill = clarity),
+    position = "dodge"
+    )
+
+# create a function that prints out the number of missing values in a dataframe
+display_missing <- function(df) {
+  n <- sum(is.na(df))
+  cat("Missing values: ", n, "\n", sep = "")
+  
+  return(invisible(df))
+  }
+
+# count missing values for ONS Mortality dataset from the NHSRdatasets package
+display_missing(ons_mortality) # result is 197
